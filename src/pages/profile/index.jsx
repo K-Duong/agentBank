a2bac698e454e23b59aa7025627b9d5cfe08b4cf
Duong TRINH <khanhduong.dav@gmail.com>
2024-07-12 13:21:57 +0200

    fix bug loading state in profilePage

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUserState } from "../../hooks/useUserState";
 import FormEditName from "../../components/form-edit-profile";
@@ -11,54 +11,72 @@ function ProfilePage() {
   const listOfAccounts = [
     {
       title: "Argent Bank Checking (x8349)",
-      availableBalance: 2082.79
+      availableBalance: 2082.79,
     },
     {
       title: "Argent Bank Savings (x6712)",
-      availableBalance: 10928.42
+      availableBalance: 10928.42,
     },
     {
       title: "Argent Bank Credit Card (x8349)",
-      availableBalance: 184.30
-    }
-  ]
+      availableBalance: 184.3,
+    },
+  ];
   const [activatedForm, setActivatedForm] = useState(false);
-  const {isLoading, user} = useUserState();
+  const { isLoading, user } = useUserState();
   const navigate = useNavigate();
 
   const handleNavigateToTransactionPage = () => {
-    navigate("/transactions")
-  }
+    navigate("/transactions");
+  };
 
   const handleOpenForm = () => {
     setActivatedForm(true);
   };
 
-  if (isLoading) return <h1>Loading...</h1>;
+  const ProfileContent = () => {
+    return (
+      <>
+        <div className="header">
+          <h1>
+            Welcome back
+            <br />
+            {!activatedForm && `${user.firstName} ${user.lastName}!`}
+          </h1>
+          {activatedForm ? (
+            <FormEditName
+              firstName={user.firstName}
+              lastName={user.lastName}
+              formState={setActivatedForm}
+            >
+              {" "}
+            </FormEditName>
+          ) : (
+            <Button className="btn-edit-name" handleClick={handleOpenForm}>
+              Edit Name
+            </Button>
+          )}
+        </div>
+        {listOfAccounts.map((account) => (
+          <Account
+            title={account.title}
+            availableBalance={account.availableBalance.toLocaleString("en-US")}
+            handleClick={handleNavigateToTransactionPage}
+            key={account.title}
+          ></Account>
+        ))}
+      </>
+    );
+  };
 
   return (
     <div className="profile-wrapper">
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          {!activatedForm && `${user.firstName} ${user.lastName}!`}
-        </h1>
-        {activatedForm ? (
-          <FormEditName
-            firstName={user.firstName}
-            lastName={user.lastName}
-            formState={setActivatedForm}
-          >
-            {" "}
-          </FormEditName>
-        ) : (
-          <Button className="btn-edit-name" handleClick={handleOpenForm}>
-            Edit Name
-          </Button>
-        )}
-      </div>
-      {listOfAccounts.map(account => <Account title={account.title} availableBalance={account.availableBalance.toLocaleString("en-US")} handleClick={handleNavigateToTransactionPage} key={account.title} ></Account>)}
+      {isLoading ? (
+        <h1 className="loading-text">Loading...</h1>
+      ) : null}
+      {
+        !!(user?.firstName && user?.lastName) ? <ProfileContent/> : null
+      }
     </div>
   );
 }
